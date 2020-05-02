c92a32684df9e9710b6c30c2b3d23cd952a92645
Hyde Liao <kcritkernel@gmail.com>
2020-05-02 12:33:57 +0800

    [java] add urlclassloader general snippet

---
+++ java/urlclassloader/general/src/main/java/snippetlab/java/urlclassloader/general/pojos/Cean.java
@@ -0,0 +1,10 @@
+package snippetlab.java.urlclassloader.general.pojos;
+
+public class Cean
+{
+	public void
+		saySomething(String str)
+	{
+		System.out.println("[" + str + "]" + "from loaded [Cean] class !!!");
+	}
+}
