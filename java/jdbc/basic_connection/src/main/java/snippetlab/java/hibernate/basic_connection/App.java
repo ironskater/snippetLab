3caa7ff267a5daaac7b4201bcdc5411c48cec6a7
Hyde Liao <kcritkernel@gmail.com>
2020-05-30 17:01:24 +0800

    [java][jdbc] add basic_connection

---
+++ java/jdbc/basic_connection/src/main/java/snippetlab/java/hibernate/basic_connection/App.java
@@ -0,0 +1,27 @@
+package snippetlab.java.hibernate.basic_connection;
+
+import java.sql.DriverManager;
+
+public class App
+{
+	public static void
+		main( String[] args )
+	{
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker";
+		String user = "hyde";
+		String pw = "hyde";
+
+		try
+		{
+			System.out.println("Connecting to Database: " + jdbcUrl);
+
+			DriverManager.getConnection(jdbcUrl, user, pw);
+
+			System.out.println("Connnection successful!!!");
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+}
