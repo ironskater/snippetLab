767422bc0230ff711527a58db9c587ad79458d03
Hyde Liao <kcritkernel@gmail.com>
2020-05-30 23:04:59 +0800

    [java][hibernate] add basic_update

---
+++ java/hibernate/basic_update/src/main/java/snippetlab/java/hibernate/basic_update/App.java
@@ -0,0 +1,70 @@
+package snippetlab.java.hibernate.basic_update;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App
+{
+	public static void
+		main( String[] args )
+	{
+		// create session factory
+		/**
+		 * [Trouble shooting] could not locate cfg.xml for hibernate
+		 *
+		 * "/home/hyde/ssd/atelier/snippetLab/java/hibernate/general/src/hibernate.cfg.xml" can't be used because
+		 * because of configure() method waits for class path resource (loaded by a classloader) or URL.
+		 *
+		 * Refer to https://stackoverflow.com/questions/47069676/could-not-locate-cfg-xml-for-hibernate
+		 */
+		SessionFactory factory = new Configuration()
+									.configure()
+									.addAnnotatedClass(Student.class)
+									.buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+			// create a student object
+			System.out.println("Creating new student object...");
+			Student tempStudent1 = new Student("hyde1", "firstHibernate", "yaya");
+
+			// start a transaction
+			session.beginTransaction();
+
+			// save the student object
+			System.out.println("Saving the student...");
+			session.save(tempStudent1);
+
+			// commit transaction
+			// After executing save(), DB still not has value until completing commit()
+			session.getTransaction().commit();
+
+			// update ----------------------------------------------------------
+
+			session = factory.getCurrentSession();
+
+			// Different to Normal SQL, in hibernate, even though doing querying, it still need a transaction.
+			session.beginTransaction();
+
+			System.out.println("find myStudent by Id: " + tempStudent1.getId());
+
+			Student myStudent = session.get(Student.class,
+											tempStudent1.getId());
+
+			System.out.println("======================= Before update, myStudent: " + myStudent.toString());
+
+			myStudent.setLastName("updateHibernate");
+
+			session.getTransaction().commit();
+
+			System.out.println("Done!!!!");
+			System.out.println("======================= After update, myStudent: " + myStudent.toString());
+		}
+		finally {
+			factory.close();
+		}
+	}
+}
