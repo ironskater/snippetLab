f0e2a2a7286eced7ac9610f2a5bfd93c6e2df7ce
Hyde Liao <kcritkernel@gmail.com>
2020-06-22 01:12:08 +0800

    [java][flink] modify data_server so that it can generate data with timestamp

--- java/flink/data_server/src/main/java/snippetlab/java/flink/data_server/App.java
+++ java/flink/data_server/src/main/java/snippetlab/java/flink/data_server/App.java
@@ -5,11 +5,54 @@ import java.io.FileReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Random;
 
 public class App
 {
 	public static void
 		main( String[] args ) throws Exception
+	{
+		App server = new App();
+
+		// server.createFixedData();
+		server.createTimestampData();
+	}
+
+	private void
+		createTimestampData() throws Exception
+	{
+		ServerSocket listener = new ServerSocket(9527);
+		try{
+				System.out.println("Start Listening...");
+				Socket socket = listener.accept();
+				System.out.println("Got new connection: " + socket.toString());
+				try {
+					PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+					Random rand = new Random();
+					while (true){
+						int data = rand.nextInt(100);
+						String dataWithTimestamp = "" + System.currentTimeMillis() + "," + data;
+						System.out.println(dataWithTimestamp);
+						/* <timestamp>,<random-number> */
+						out.println(dataWithTimestamp);
+						Thread.sleep(50);
+					}
+
+				} finally{
+					System.out.println("Start closing server...");
+					socket.close();
+				}
+
+		} catch(Exception e ){
+			e.printStackTrace();
+		} finally{
+
+			listener.close();
+		}
+	}
+
+	private void
+		createFixedData() throws Exception
 	{
 		ServerSocket listener = new ServerSocket(9527);
 		try{
@@ -23,7 +66,7 @@ public class App
 					PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 					String line;
 					while ((line = br.readLine()) != null){
-
+						System.out.println(line);
 						out.println(line);
 						Thread.sleep(50);
 					}
