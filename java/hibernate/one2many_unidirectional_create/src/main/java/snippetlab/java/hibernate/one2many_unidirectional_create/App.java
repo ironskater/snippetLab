9c9997881391d9525b2cab0185b5b1b56d04bc8a
Hyde Liao <kcritkernel@gmail.com>
2020-07-05 22:32:38 +0800

    [java][hibernate] add one2many_unidirectional_create

---
+++ java/hibernate/one2many_unidirectional_create/src/main/java/snippetlab/java/hibernate/one2many_unidirectional_create/App.java
@@ -0,0 +1,55 @@
+package snippetlab.java.hibernate.one2many_unidirectional_create;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App
+{
+	public static void
+		main( String[] args )
+	{
+		// create session factory
+		/**
+		 * [Trouble shooting] could not locate cfg.xml for hibernate
+		 *
+		 * "/home/hyde/ssd/atelier/snippetLab/java/hibernate/general/src/hibernate.cfg.xml" can't be used because
+		 * because of configure() method waits for class path resource (loaded by a classloader) or URL.
+		 *
+		 * Refer to https://stackoverflow.com/questions/47069676/could-not-locate-cfg-xml-for-hibernate
+		 */
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Instructor.class)
+									.addAnnotatedClass(InstructorDetail.class)
+									.addAnnotatedClass(Course.class)
+									.addAnnotatedClass(Review.class)
+									.buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+			session.beginTransaction();
+
+			Course tempCourse1 = new Course("Guitar");
+			Course tempCourse2 = new Course("Bass");
+
+			tempCourse1.addReview(new Review("nice guitar lesson"));
+			tempCourse1.addReview(new Review("good guitar lesson"));
+			tempCourse2.addReview(new Review("nice bass lesson"));
+			tempCourse2.addReview(new Review("good bass lesson"));
+
+			session.save(tempCourse1);
+			session.save(tempCourse2);
+
+			session.getTransaction().commit();
+
+			System.out.println("Done!!!!!!!!!!!!!!!!!!!!!!!!!1");
+		}
+		finally {
+			session.close();
+			factory.close();
+		}
+	}
+}
---
+++ java/hibernate/one2many_unidirectional_create/src/main/java/snippetlab/java/hibernate/one2many_unidirectional_create/InstructorDetail.java
@@ -0,0 +1,64 @@
+package snippetlab.java.hibernate.one2many_unidirectional_create;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="instructor_detail")
+public class InstructorDetail
+{
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+
+	@Column(name="youtube_channel")
+	private String youtubeChannel;
+
+	@Column(name="hobby")
+	private String hobby;
+
+	public InstructorDetail() {}
+
+	public InstructorDetail(String youtubeChannel, String hobby) {
+		this.youtubeChannel = youtubeChannel;
+		this.hobby = hobby;
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getYoutubeChannel() {
+		return this.youtubeChannel;
+	}
+
+	public void setYoutubeChannel(String youtubeChannel) {
+		this.youtubeChannel = youtubeChannel;
+	}
+
+	public String getHobby() {
+		return this.hobby;
+	}
+
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+
+	@Override
+	public String toString() {
+		return "{" +
+			" id='" + getId() + "'" +
+			", youtubeChannel='" + getYoutubeChannel() + "'" +
+			", hobby='" + getHobby() + "'" +
+			"}";
+	}
+}
