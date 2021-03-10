76ea59e062c544e1203cf646450fdbfcccb7ef77
Hyde <hyde_liao@acbel.com>
2021-03-10 17:36:46 +0800

    [python][matplot] add how to change imgsize in notebook

--- python/matplot/main.py
+++ python/matplot/main.py
@@ -1,5 +1,9 @@
 from typing import List
 from matplotlib import pyplot as plt
+# %matplotlib inline
+# %matplotlib qt <- create img outside notebook
+
+plt.figure(figsize=(4, 3)) # change size, maybe only works in notebook
 
 def plot_xy_line(title:str, x_label:str, y_label:str, x:List, y:List, labels:List):
 	for idx in range(len(y)):
@@ -18,9 +22,8 @@ def plot_xy_line(title:str, x_label:str, y_label:str, x:List, y:List, labels:Lis
 	plt.legend()
 	plt.show()
 
-plot_xy_line('TITLE', 'X_AXIS', 'Y_AXIS', [], [[2, 2, 2, 3]], ['a', 'b'])
-plot_xy_line('TITLE', 'X_AXIS', 'Y_AXIS', [], [[2, 2, 2, 3], [7, 4, 2, 1]], ['a', 'b'])
+# plot_xy_line('TITLE', 'X_AXIS', 'Y_AXIS', [], [[2, 2, 2, 3]], ['a', 'b'])
+# plot_xy_line('TITLE', 'X_AXIS', 'Y_AXIS', [], [[2, 2, 2, 3], [7, 4, 2, 1]], ['a', 'b'])
 
 ticks = [x+2 for x in range(4)]
-
 plot_xy_line('TITLE', 'X_AXIS', 'Y_AXIS', [ticks, ticks], [[2, 2, 2, 2], [5, 5, 5, 5]], ['a', 'b'])
