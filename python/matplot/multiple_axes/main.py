9c71ab51dca5786244507bcbc3d213bdebaa994e
Hyde <hyde_liao@acbel.com>
2021-03-18 11:45:28 +0800

    [python][matplotlib] add plt.subplots to plot multiple_axes

--- python/matplot/multiple_axes/main.py
+++ python/matplot/multiple_axes/main.py
@@ -53,5 +53,21 @@ ax2.set_title("tan wave")
 plt.show()
 
 
+# ===== Using plt.subplots to plot multiple axes =====
+# plt.subplots returns a figure object and a tuple containing axes objects equal to nrows*ncols.
+# Each axes object is accessible by its index.
+fig,a =  plt.subplots(nrows = 2, ncols = 2)
 
+a[0][0].plot(x, x*x)
+a[0][0].set_title('square')
 
+a[0][1].plot(x,np.sqrt(x))
+a[0][1].set_title('square root')
+
+a[1][0].plot(x,np.exp(x))
+a[1][0].set_title('exp')
+
+a[1][1].plot(x,np.log10(x))
+a[1][1].set_title('log')
+
+plt.show()
